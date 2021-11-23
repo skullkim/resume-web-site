4987bb2aea5ffbd3d5cee87f3d901b7a975a5f04
skullKim <kyk990328@gmail.com>
2021-11-23 16:31:36 +0900

    추가: 첫 페이지 green 완료

--- components/FirstPage.tsx
+++ components/FirstPage.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+import {ImagePath} from "../lib/datatPath";
+
+const FirstPageImage = styled.img`
+  position: absolute;
+  width: 100%;
+`;
 
 function FirstPage () {
   return (
-    <></>
+    <FirstPageImage src={ImagePath.FirstPageImage} alt='first page image'/>
   );
 }
 
